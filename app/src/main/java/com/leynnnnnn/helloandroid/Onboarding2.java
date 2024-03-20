badc975e37b6593123e5faae7f3425eb2b00a0ae
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-03-20 21:25:11 +0800

    finished onboarding pages

--- app/src/main/java/com/leynnnnnn/helloandroid/Onboarding2.java
+++ app/src/main/java/com/leynnnnnn/helloandroid/Onboarding2.java
@@ -10,21 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Onboarding1 extends AppCompatActivity {
+public class Onboarding2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onboarding1);
+        setContentView(R.layout.activity_onboarding2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void nextPage(View v) {
-        Intent i = new Intent(this, Onboarding2.class);
+    public void nextPage3(View v) {
+        Intent i = new Intent(this, Onboarding3.class);
         startActivity(i);
     }
+
 }
